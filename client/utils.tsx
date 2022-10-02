47b337b3fb57330c21a4cecaa33c2e4cd915f59a
Logan Barnett <logustus@gmail.com>
2022-10-02 14:43:29 -0700

    WIP traversal to find a selection in a path

--- client/utils.tsx
+++ client/utils.tsx
@@ -30,3 +30,47 @@ export const selectionTitle = (
       || `Option '${selection.optionKey}' not found!`
     )
 }
+
+type Traversal<A> = (a: A) => A | null | undefined
+/**
+ * Finds data in a deeply nested structure.
+ *
+ * The best way to accomplish a deep find is with a path. Just like how we might
+ * traverse a file hierarchy to find a file, in order to do something with that
+ * file we will need the file's path. So deep find must return a path-like
+ * entity that can be applied to get the desired data.
+ *
+ * If a traverse function can be used to access any item within a given layer
+ * of a tree, then the path-like entity is a series of traversal functions,
+ * ready to be applied for actually gathering the data.
+ */
+export const deepFind = <A,>(
+  traversals: ReadonlyArray<Traversal<A>>,
+  compare: (x: A, y: A) => boolean,
+  traverse: (a: A) => ReadonlyArray<A>,
+  current: A,
+  desired: A,
+): Array<Traversal<A>> => {
+  const self = traversals.concat([(parent) => {
+    return traverse(parent).find(compare.bind(null, current))
+  }])
+  if(compare(desired, current)) {
+    return self
+  } else {
+    return traverse(current).reduce((acc, child) => {
+      const ts = deepFind(self, compare, traverse, child, desired)
+      if(ts != traversals) {
+        return ts
+      } else {
+        return acc
+      }
+    }, [] as Array<Traversal<A>>)
+  }
+}
+
+/**
+ * Modifies a deeply nested structure in an immutable fashion.
+ */
+export const deepModify = () => {
+
+}
