a88b63d513ad20eab55c70572ae709c6b14d82d1
Logan Barnett <logustus@gmail.com>
2022-10-22 20:24:50 -0700

    use explicit types to solidify type tests

--- client/result-test.ts
+++ client/result-test.ts
@@ -10,21 +10,33 @@ class HappinessError extends Error {
     return true
   }
 }
-  // .map(s => s.toUpperCase())
-const r0 = Result.ok('foo')
-const r1 = r0
+// ok gives us the type based on the input.
+const r0: ResultType<string, Error> = Result.ok('foo')
+// map changes the first type parameter (the data type) of result type.
+const r1: ResultType<number, Error> = r0
   .map(s => parseInt(s))
 
-const r2 = r1
+// mapErr changes the type of the error type.
+const r2: ResultType<number, HappinessError> = r1
   .map(i => i * 2)
   .mapErr(e => new HappinessError())
 
-const r3 = foobar()
-r3
+const r3: ResultType<number, Error> = foobar()
+const r4: ResultType<string, HappinessError> = r3
   .map(i => i.toString())
   .map(s => s.toUpperCase())
   .orElse(e => Result.ok(e.message))
   .andThen(i => Result.err(new HappinessError(i.toString())))
   .andThen(_i => foobar())
   .andThen(_e => Result.ok('happy'))
+  // This should probably reset our error type, but I don't observe that here.
+  // That's because of the any typing on the error. So it gets lost.
+  .orElse(_e => Result.ok('happy'))
+
+foobar()
+  // This might seem a little weird but we can't change the type here. This is
+  // how Rust's Result works too, so we're not out of line. This is for the
+  // simple reason that the type would be A | B because we don't know if the
+  // Result is ok or err.
+  // @ts-expect-error
   .orElse(_e => Result.ok('happy'))
