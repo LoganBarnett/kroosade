66827925fff1e4283418333ae7b3372e42a43894
Logan Barnett <logustus@gmail.com>
2022-10-24 21:01:28 -0700

    add focus capability to extant selections

--- client/extant-selection-editor.tsx
+++ client/extant-selection-editor.tsx
@@ -3,10 +3,17 @@
  * or removing.
  */
 
-import React, { type FC, type ReactNode, type ReactElement } from 'react'
-import { type ExtantSelection, type AppOption, } from './model'
+import React, { type FC, type ReactNode, type ReactElement, useContext } from 'react'
+import { selectionFocusAction } from './app-reducer'
 import buttonFn from './button'
 import buttonStyles from './button.module.css'
+import {
+  type AppOption,
+  type AppSelection,
+  type ExtantSelection,
+} from './model'
+import { Context } from './reducer-provider'
+import { selectionTitle } from './utils'
 import visibilityFn from './visible'
 
 export type Props = {
@@ -17,16 +24,20 @@ export type Props = {
 export type Component = FC<Props>
 
 export default (className: string): FC<Props> => {
-  const AddButton = buttonFn(buttonStyles.add)
+  const FocusButton = buttonFn(buttonStyles.focus)
   const DeleteButton = buttonFn(buttonStyles.remove)
   const Visibility = visibilityFn('display')
   const component = (props: Props): ReactElement => {
+    const { dispatch } = useContext(Context)
+    const focusFn = (s: AppSelection) => dispatch(selectionFocusAction(s))
     return <fieldset className={className}>
-      <Visibility visible={!props.selection.selected}>
-        <AddButton onClick={() => {}}>add</AddButton>
-      </Visibility>
       <Visibility visible={props.selection.selected}>
-        <DeleteButton onClick={() => {}}>remove</DeleteButton>
+        <FocusButton onClick={() => focusFn(props.selection)}>
+          focus
+        </FocusButton>
+        <DeleteButton onClick={() => {}}>
+          remove {selectionTitle(props.options, props.selection)}
+        </DeleteButton>
       </Visibility>
     </fieldset>
   }
