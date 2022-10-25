8af1c6425757d5e95e3bfc4e49c78f930bfe76ad
Logan Barnett <logustus@gmail.com>
2022-10-24 21:07:05 -0700

    hide focus button for focused in roster view

--- client/focus-selection.tsx
+++ client/focus-selection.tsx
@@ -1,7 +1,14 @@
-import React, { type FC, type ReactElement, type ReactNode } from 'react'
+import React, {
+  type FC,
+  type ReactElement,
+  type ReactNode,
+  useContext,
+} from 'react'
+import { selectionFocusAction } from './app-reducer'
 import buttonFn from './button'
 import buttonStyles from './button.module.css'
 import { type AppSelection } from './model'
+import { Context } from './reducer-provider'
 
 export type Props = {
   children: ReactNode,
@@ -12,10 +19,15 @@ export type Props = {
 export default (className: String): FC<Props> => {
   const FocusButton = buttonFn(buttonStyles.focus)
   const component = (props: Props): ReactElement => {
+    const { state, dispatch } = useContext(Context)
+    const focusFn = (s: AppSelection) => dispatch(selectionFocusAction(s))
     return <>
-      <FocusButton onClick={() => props.focus(props.selection)}>
-        {props.children}
-      </FocusButton>
+      { state.focus?.id != props.selection.id
+        ? <FocusButton onClick={() => focusFn(props.selection)}>
+            {props.children}
+          </FocusButton>
+        : <></>
+        }
     </>
   }
   component.displayName = 'FocusSeleciton'
