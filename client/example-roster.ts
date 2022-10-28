1b153d0ef328fac9d6cedfe3ccd02f0d94c35eb4
Logan Barnett <logustus@gmail.com>
2022-10-27 22:28:07 -0700

    remove outdated example data
    I don't want to repair this, and it's just a couple of clicks to add
    back in.

--- client/example-roster.ts
+++ client/example-roster.ts
@@ -6,39 +6,6 @@ export const roster: AppSelection = {
       children: [
         {
           children: [
-            {
-              children: [
-                {
-                  children: [],
-                  id: '5',
-                  kind: 'numeric-selection',
-                  name: null,
-                  optionKey: 'dire-avengers-unit-size',
-                  value: 4,
-                },
-                {
-                  children: [],
-                  id: '6',
-                  kind: 'exclusive-selection',
-                  name: null,
-                  optionKey: 'dire-avengers-exarch-power',
-                  selected: 'dire-avengers-exarch-power-stand-firm',
-                },
-                {
-                  children: [],
-                  id: '7',
-                  kind: 'exclusive-selection',
-                  name: null,
-                  optionKey: 'dire-avengers-exarch-wargear',
-                  selected: 'dire-avengers-exarch-wargear-two-avenger-shuriken-catapults',
-                },
-              ],
-              id: '4',
-              kind: 'extant-selection',
-              name: null,
-              optionKey: 'dire-avengers-unit',
-              selected: true,
-            },
           ],
           id: '3',
           kind: 'repeating-extant-selection',
