9e1f0bb6ba9cb9243593f82d8efb8cf68aa02d67
Logan Barnett <logustus@gmail.com>
2023-03-28 17:53:35 -0700

    add non-existent container
    At least we can capture our musings about it.

---
+++ client/container.ts
@@ -0,0 +1,8 @@
+/**
+ * A container is a generic container of any kind. Generally it will be a div
+ * but it could be anything. Using this allows us to integrate our styling with
+ * how we generally use components, but there's no other great advantage over
+ * using this vs. just a flat element.
+ *
+ * Maybe we don't need it at all?
+ */
