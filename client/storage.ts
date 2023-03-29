da5f1282bd482d0ab8245395dd3dcf5e90d0690e
Logan Barnett <logustus@gmail.com>
2023-03-28 17:53:55 -0700

    start a storage file
    More emptiness.

---
+++ client/storage.ts
@@ -0,0 +1,6 @@
+/*******************************************************************************
+ * Manage storage for the application, be it local or remote storage.
+ *
+ * A guiding principal of this application is that it should work without a
+ * server as much as possible, or even serve as a standalone application.
+ ******************************************************************************/
